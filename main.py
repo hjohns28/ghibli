8c34abaf41c06a26793a77ed65876e0adafd14c0
hjohns28 <hjohns28@lion.lmu.edu>
2019-08-08 15:11:34 -0700

    Just a prototype main page

---
+++ main.py
@@ -0,0 +1,35 @@
+import webapp2
+import jinja2
+import os
+from google.appengine.api import urlfetch
+import json
+from urllib import urlencode
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPageHandler(webapp2.RequestHandler):
+    def get(self):
+        template = the_jinja_env.get_template('templates/main.html')
+        self.response.write(template.render())
+
+# class RecipeDisplayHandler(webapp2.RequestHandler):
+#     def post(self):
+#         query = self.request.get('query')
+#         ingreds = self.request.get('ingreds')
+#         base_url = 'http://www.recipepuppy.com/api/?'
+#         params = { 'i' : ingreds, 'q': query }
+#         response = urlfetch.fetch(base_url + urlencode(params)).content
+#         results = json.loads(response)
+#
+#         template = the_jinja_env.get_template('templates/recipe.html')
+#         self.response.write(template.render({
+#             "results": results
+#         }))
+
+# the app configuration section
+app = webapp2.WSGIApplication([
+    ('/', MainPageHandler),
+], debug=True)
