4e08b72825a0d6f219803f872f355f3b9337506c
hjohns28 <hjohns28@lion.lmu.edu>
2019-08-08 17:22:01 -0700

    Basic search

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
+
+import os
+import json
 import webapp2
 import jinja2
-import os
-from google.appengine.api import urlfetch
-import json
 from urllib import urlencode
+from google.appengine.api import urlfetch
 
-the_jinja_env = jinja2.Environment(
+jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
-        template = the_jinja_env.get_template('templates/main.html')
+        template = jinja_env.get_template('templates/main.html')
         self.response.write(template.render())
 
-# class RecipeDisplayHandler(webapp2.RequestHandler):
-#     def post(self):
-#         query = self.request.get('query')
-#         ingreds = self.request.get('ingreds')
-#         base_url = 'http://www.recipepuppy.com/api/?'
-#         params = { 'i' : ingreds, 'q': query }
-#         response = urlfetch.fetch(base_url + urlencode(params)).content
-#         results = json.loads(response)
-#
-#         template = the_jinja_env.get_template('templates/recipe.html')
-#         self.response.write(template.render({
-#             "results": results
-#         }))
+class SearchHandler(webapp2.RequestHandler):
+    def post(self):
+        filter = self.request.get('filter')
+        base_url = 'https://ghibliapi.herokuapp.com/{}'.format(filter)
+        response = json.loads(urlfetch.fetch(base_url).content)
+        template = jinja_env.get_template('templates/results.html')
+        self.response.write(template.render({ 'response': response }))
 
-# the app configuration section
+        # params = {
+        #     'q': self.request.get('query'),
+        #     'i': self.request.get('ingredients')}
+        # response = json.loads(urlfetch.fetch(base_url + urlencode(params)).content)
+
+
 app = webapp2.WSGIApplication([
     ('/', MainPageHandler),
+    ('/search', SearchHandler),
 ], debug=True)
